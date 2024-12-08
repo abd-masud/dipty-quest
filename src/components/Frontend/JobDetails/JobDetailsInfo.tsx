b2666bb5701d76f9a125dfb3cee6af56f435a8b4
abdMasud <abdmasud.2000@gmail.com>
2024-12-08 19:52:45 +0600

    job details

--- src/components/Frontend/JobDetails/JobDetailsInfo.tsx
+++ src/components/Frontend/JobDetails/JobDetailsInfo.tsx
@@ -1,7 +1,127 @@
+import Link from "next/link";
+import { FaGraduationCap, FaMoneyCheckAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdBusinessCenter } from "react-icons/md";
+
 export const JobDetailsInfo = () => {
   return (
     <main>
-      <div></div>
+      <div className="max-w-screen-xl mx-auto px-4 py-5">
+        <div className="mb-5">
+          <h2 className="font-bold text-[30px]">Executive (Sales & Service)</h2>
+          <p>Global Medical Engineering (BD) Ltd.</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <p className="text-[14px] font-bold">
+            Deadline: <span className="text-red-500 ml-1">10th Dec, 2024</span>
+          </p>
+          <Link
+            className="border border-[#131226] hover:text-[#131226] hover:bg-transparent py-2 px-5 flex justify-center items-center text-white bg-[#131226] transition duration-300"
+            href="/"
+          >
+            Apply Now
+          </Link>
+        </div>
+      </div>
+      <div className="border-y py-5 bg-gray-100">
+        <div className="max-w-screen-xl mx-auto px-4 grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+          <div className="flex items-center gap-2 py-1 px-3">
+            <div className="bg-blue-50 h-12 w-12 flex justify-center items-center rounded-full border border-blue-600">
+              <FaMoneyCheckAlt className="text-xl text-blue-600" />
+            </div>
+            <div className="ml-2">
+              <p>Negotiable</p>
+              <p className="text-[14px]">Monthly Salary</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 py-1 px-3">
+            <div className="bg-green-50 h-12 w-12 flex justify-center items-center rounded-full border border-green-600">
+              <MdBusinessCenter className="text-xl text-green-600" />
+            </div>
+            <div className="ml-2">
+              <p>Full time</p>
+              <p className="text-[14px]">Employment Type</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 py-1 px-3">
+            <div className="bg-purple-50 h-12 w-12 flex justify-center items-center rounded-full border border-purple-600">
+              <FaGraduationCap className="text-xl text-purple-600" />
+            </div>
+            <div className="ml-2">
+              <p>Graduation</p>
+              <p className="text-[14px]">Education Qualification</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 py-1 px-3">
+            <div className="bg-orange-50 h-12 w-12 flex justify-center items-center rounded-full border border-orange-600">
+              <FaLocationDot className="text-xl text-orange-600" />
+            </div>
+            <div className="ml-2">
+              <p>Anywhere</p>
+              <p className="text-[14px]">Job Location</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-screen-xl mx-auto px-4 py-5">
+        <h2 className="text-[24px] font-bold">Job Description</h2>
+        <h3 className="text-[18px] font-bold mt-3">Company Overview</h3>
+        <p>
+          Global Medical Engineering (BD) Ltd is the leading medical and health
+          equipment supplier in Bangladesh. We are incorporated as a joint-stock
+          company, established in 2007. Our mission is to provide the best
+          service and widest the range of high-quality products in the health
+          and medical sector. We serve our customers to every corner of the
+          country, with the excellent world-renowned medical diagnostic reagents
+          and other consumables, used in all fields of medical sectors.
+        </p>
+        <h3 className="text-[18px] font-bold mt-3">Responsibilities</h3>
+        <p>
+          # Contributes information, ideas, and research to help develop
+          marketing strategies.
+        </p>
+        <p>
+          # Helps in detail, design, and implement marketing plans for each
+          product or service being offered.
+        </p>
+        <p>
+          # Answers questions from clients about product and service benefits.
+        </p>
+        <p>
+          # Maintains excellent relationships with clients through superior
+          customer service.
+        </p>
+        <p># Tracks sales data and works to meet quotas or sales team goals.</p>
+        <p>
+          # Set up meetings between client decision makers and company’s
+          practice leaders/Principals.
+        </p>
+        <p># Price Negotiation and Deal Closing.</p>
+        <h2 className="text-[24px] font-bold mt-5">Education</h2>
+        <p>Minimum Qualification: Bachelor Degree</p>
+        <p>Preferred Qualification: Undefined Degree</p>
+        <h2 className="text-[24px] font-bold mt-5">Experience</h2>
+        <p>At least 0 years of experience</p>
+        <p>Preferred number of years of experience: 1 years</p>
+        <h2 className="text-[24px] font-bold mt-5">Job Requirements</h2>
+        <h3 className="text-[18px] font-bold mt-3">Qualification</h3>
+        <p>
+          # Bachelor’s Degree in Marketing, Business or a Related Field. Diploma
+          in Electro Medical/Bio-Medical.
+        </p>
+        <p># Minimum 0-1 year experience in the relevant field.</p>
+        <p># At least 6 months of job experience in the Health Sector.</p>
+        <h3 className="text-[18px] font-bold mt-3">Additional Requirements</h3>
+        <p># Having experience in Health Sector will get preference.</p>
+        <p># Good communication skill.</p>
+        <h2>Compensation & Other Benefits</h2>
+        <p>Salary : Negotiable</p>
+        <p>Benefits</p>
+        <h2 className="text-[24px] font-bold mt-5">Employment Status</h2>
+        <p>Full-time</p>
+        <h2 className="text-[24px] font-bold mt-5">Job Location</h2>
+        <p>All over Bangladesh</p>
+      </div>
     </main>
   );
 };
--- src/components/Frontend/TermsConditions/TermsConditionsInfo.tsx
+++ src/components/Frontend/TermsConditions/TermsConditionsInfo.tsx
@@ -1,7 +1,297 @@
 export const TermsConditionsInfo = () => {
   return (
-    <main>
-      <div></div>
+    <main className="max-w-screen-xl mx-auto px-4 md:py-10 py-5">
+      <div>
+        <p className="mb-5">
+          Please review the following terms and conditions carefully before
+          using DiptyQuest.
+        </p>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Disclaimer</h2>
+          <p>
+            Our organization accepts no responsibility for the acquisition of
+            personal information by a third party in the following situations:
+          </p>
+          <p>
+            (1) When a user provides personal information to a specific company
+            through the functions of this service or another means.
+          </p>
+          <p>
+            (2) If the user is unintentionally identified based on activity
+            information or information entered onto this service.
+          </p>
+          <p>
+            (3) When a user provides personal information, that information is
+            used in an external service linked to this one.
+          </p>
+          <p>
+            (4) If a third party other than the user receives personally
+            identifiable information (ID, password, etc.).
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Protecting private data:</h2>
+          <p>
+            (1) The acquisition, use, and disclosure of personal information
+          </p>
+          <p>
+            Regarding the collection, use, and provision of personal
+            information, we will manage it correctly within the extent of the
+            purpose of use mentioned in "Handling of Personal Information",
+            taking into account the content and scale of the information
+            service, among other things. Our organization will not use acquired
+            or entrusted personal information for any purpose other than those
+            specified in the terms and conditions agreed upon by users. If
+            personal information is to be used for reasons other than the
+            intended use, the individual's agreement must be looked for in
+            advance.
+          </p>
+          <p>(2) Correctly handling of personal information</p>
+          <p>
+            * We shall maintain an adequate and acceptable safety management
+            system to prevent unauthorized access to personal information, as
+            well as loss, destruction, falsification, leaking, and so on.
+          </p>
+          <p>
+            * Access to personal information shall be limited to the bare
+            minimum of officials and employees.
+          </p>
+          <p>
+            * When we outsource the management of personal information, we shall
+            sign a confidentiality agreement and execute the appropriate audits
+            in order to ensure its efficiency.
+          </p>
+          <p>
+            * Our organization respects individuals rights for personal
+            information and will respond to requests for disclosure, correction,
+            or deletion of personal information using reasonable methods.
+          </p>
+          <p>
+            (3) Our organization may share sensitive information to its
+            affiliated parties.
+          </p>
+          <p>
+            Such as its own officers, employees, attorneys or tax accountants,
+            consultants, and advisers, but only to the degree required to
+            perform this service or as indicated in these Terms of Use. The
+            information will not be disclosed or leaked to any other third party
+            without the user company's prior written consent, unless disclosure
+            is required by law or by an order of a court or government agency,
+            and it will not be used for purposes other than providing this
+            service or for purposes outside the scope of these Terms of Use.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">
+            Intellectual property rights, including copyright and other property
+            rights:
+          </h2>
+          <p>
+            You agree that DiptyQuest, its affiliates, directors, officers,
+            employees, agents, suppliers, or licensors own all materials,
+            products, and services available on this website, including
+            copyrights, trade secrets, trademarks, patents, and other
+            intellectual property. You also undertake not to replicate or
+            transfer DiptyQuest's intellectual property in any form, including
+            electronic, digital, or new trademark registrations. You provide
+            DiptyQuest a royalty-free, non-exclusive right to display, use,
+            copy, transmit, and broadcast the content you submit and publish.
+            For intellectual property disputes, you should approach the
+            corporation to reach an arrangement.
+          </p>
+          <p>
+            1. The organization shall own the copyright to the deliverables
+            arising from the outsourced work performed by the Company in
+            accordance with this agreement, and the organization shall grant the
+            User the right to utilize the work to the extent required for the
+            User to use the service efficiently.
+          </p>
+          <p>
+            2. The organization owns the copyright and all other intellectual
+            property rights to existing components to which it has previously
+            had rights or that are regularly utilized in comparable types of
+            work.
+          </p>
+          <p>
+            3. The organization will be able to openly state that the
+            Deliverables were generated by itself. Users will agree in advance
+            that the organization may utilize such deliverables (including those
+            developed by the User) as needed for DiptyQuest.org and the
+            organization's advertising purposes.
+          </p>
+          <p>
+            4. In addition to what is provided for in this article, if an
+            invention, device, design, or other result (hereinafter referred to
+            as a "invention, etc.") is generated in the process of performing
+            commissioned work, a patent or utility model registration for the
+            invention, the right to receive design registration, etc., and the
+            industrial property rights and other intellectual property rights
+            (including rights related to know-how, etc.) acquired based on such
+            right.
+          </p>
+          <p>
+            5. The User is not liable for any third party's copyrights, patent
+            rights, design rights, or other intellectual property rights in
+            relation to materials, data, and so on submitted by the User to the
+            organization in order for the commissioned work to be completed. You
+            agree that you will not infringe on any proprietary rights.
+          </p>
+          <p>
+            6. When utilizing this service and employing materials from our
+            firm, the materials will not be transferred, but will be delivered
+            to the user's service. Users are banned from making secondary use of
+            the materials.
+          </p>
+          <p>
+            7. If a third party files a complaint, demand, injunction, or other
+            claim alleging violation of intellectual property rights while the
+            user uses the product, the user and the organization shall discuss
+            and resolve the issue.
+          </p>
+          <p>
+            8. The person owns the intellectual property rights to images,
+            logos, documents, and other items given by information posters to
+            our organization and to which the information poster previously had
+            intellectual property rights. However, our organization owns the
+            intellectual property rights to employment advertising and all of
+            the content included within them (including information, documents,
+            pictures, images, videos, and so on), with the exception of the
+            property rights of information posters. The author will not exercise
+            any legal rights in this regard.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">
+            The user and the job publishing Company pledge the other party that
+            they will not engage in any of the following actions, either
+            directly or via a third party...
+          </h2>
+          <p>1. Violent demands.</p>
+          <p>2. Unreasonable requirements exceed legal responsibilities.</p>
+          <p>3. Threatening or violent behavior during transactions.</p>
+          <p>
+            4. Spreading rumors, employing false tactics or force to undermine
+            the other party's credibility, or interfering with their company.
+          </p>
+          <p>5. Other acts comparable to those listed above.</p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2>
+            If the other party falls under any of the following items or is
+            reasonably believed to fall under any of the following, the user
+            company or our organization has the right to cancel all or part of
+            this agreement in writing without notice...
+          </h2>
+          <p>
+            1. In the event of a breach of paragraph 1 or the previous paragraph
+          </p>
+          <p>
+            2. Against anti-social forces, such as when you or your officers,
+            for example, participate in any type of transaction with them, such
+            as investing, lending, supplying cash or services, and so on. If the
+            individual has a connection in which the person is regarded as being
+            involved in the provision of finances, benefits, etc.
+          </p>
+          <p>
+            3. Relationships in which the firm or its executives, etc. are
+            regarded to be unjustly employing anti-social forces, such as
+            pursuing false gains for the company or a third party, or causing
+            harm to a third party.
+          </p>
+          <p>
+            4. If the individual, his or her officers, etc., have a socially
+            unacceptable contact with antisocial forces.
+          </p>
+          <p>5. In other instances, as per the previous items.</p>
+        </div>
+        <h2 className="font-bold text-[20px] mb-5">
+          If this Agreement is canceled in accordance with the provisions of the
+          preceding paragraph, neither the User Company nor the organization
+          shall be responsible for compensating the other party for any damage
+          caused by such cancellation, and the organization shall not be
+          responsible for compensating the other party for any damages incurred
+          as a result of such cancellation. It will be possible to seek
+          compensation.
+        </h2>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">About usage fees:</h2>
+          <p>
+            Until further notice, the portal is free for all companies and job
+            searchers, with the exception of some portions.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Conditions of use:</h2>
+          <p>
+            By using this website, you acknowledge that you have read and
+            reviewed this agreement and agree to abide by its terms. If you do
+            not wish to be bound by the terms of this Agreement, you may leave
+            the website. DiptyQuest only provides use and access to this
+            website, its products, and services to those who have agreed to its
+            conditions.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Age restriction:</h2>
+          <p>
+            You need to be 18 (eighteen) years old to use this website. By using
+            this website, you agree that you are at least 18 years old and can
+            legally abide by this Agreement. DiptyQuest accepts no
+            responsibility for liability arising from age misrepresentation.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">User accounts:</h2>
+          <p>
+            As a user of this website, you may be required to register with us
+            and supply personal information. You are responsible for the
+            correctness of this information, as well as the protection and
+            security of your personally identifiable information. You are also
+            liable for all actions that take place under your account or
+            password.
+          </p>
+          <p>
+            If you believe there are any potential security concerns with your
+            account on the website, please notify us immediately so that we can
+            resolve them appropriately. We have the right to terminate accounts,
+            delete material, and cancel orders at our discretion.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Applicable law:</h2>
+          <p>
+            You might be required to register with us and provide personal
+            information as a user of our website. Maintaining the safety and
+            security of your identifying information is your responsibility, as
+            is making sure that this information is accurate. Additionally, you
+            are in charge of anything that happens using your password or
+            account. Please notify us right away if you believe there may be any
+            problems with the website's account security so we can take the
+            appropriate action. At our sole discretion, we reserve the right to
+            cancel orders, delete material, and terminate accounts.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Indemnification:</h2>
+          <p>
+            You consent to indemnify DiptyQuest and its affiliates and to
+            protect DiptyQuest from any demands or legal claims resulting from
+            your improper use of our services. We retain the right to choose our
+            own counsel.
+          </p>
+        </div>
+        <div className="flex flex-col space-y-2 mb-5">
+          <h2 className="font-bold text-[20px]">Limitation on liability:</h2>
+          <p>
+            DiptyQuest disclaims all liability for any harm you may sustain from
+            misusing our website. This Agreement may be amended, modified, and
+            changed at any time by DiptyQuest. Through email, we will notify our
+            users of these changes. All previous agreements pertaining to the
+            use of this website are superseded and replaced by this agreement,
+            which is an understanding between the user and DiptyQuest.
+          </p>
+        </div>
+      </div>
     </main>
   );
 };
